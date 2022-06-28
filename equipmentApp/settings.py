a27194fbe9bd110f59b06808c40c7ed03671875b
wcqrwtqr <mohalbatati@hotmail.com>
2022-06-28 23:03:07 +0300

    second commit

--- equipmentApp/settings.py
+++ equipmentApp/settings.py
@@ -22,7 +22,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0#a=!dwxkwcwnks!ke%+czgjlb4^2$u1*g@t(#(r^a!m6$ac24'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
